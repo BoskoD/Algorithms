b7ccaf3b030b816effbf59289defe12e05987212
Bosko Danilovic <boskodanilovic@outlook.com>
2022-05-16 14:07:10 +0200

    Staircase

---
+++ Staircase/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Staircase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Result.Staircase(4);
+        }
+    }
+
+    class Result
+    {
+        public static void Staircase(int n) 
+        {
+            for (int y = n - 1; y >= 0; y--) // math vs cs
+            {
+                for (int x = 0; x < n; x++)
+                {
+                    if (x >= y)
+                    {
+                        Console.Write("#");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+}
