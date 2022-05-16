7a7127d6c5393783f81e0c9b719f289139a7c29c
Bosko Danilovic <boskodanilovic@outlook.com>
2022-05-16 15:45:30 +0200

    PlusMinus(Precission)

---
+++ PlusMinus/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PlusMinus
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] arr = new[] { 1, 1, 0, -1, -1 };
+            Result.PlusMinus(arr);
+        }
+    }
+
+    class Result
+    {
+        public static void PlusMinus(int[] arr)
+        {
+            double positives = 0;
+            double negatives = 0;
+            double zeros = 0;
+            double length = arr.Length;
+
+            for (int i = 0; i < length; i++)
+            {
+                if (arr[i] > 0) positives++;
+                else if (arr[i] < 0) negatives++;
+                else zeros++;
+            }
+
+            Console.WriteLine($"Postivies:{positives/length:F6}");
+            Console.WriteLine($"Negatives:{negatives / length:F6}");
+            Console.WriteLine($"Zeros:{zeros / length:F6}");
+        }
+    }
+}
