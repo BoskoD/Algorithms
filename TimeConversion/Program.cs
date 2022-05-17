71dc3af4fe8945f28934a0025c7fe097ebc1f6f8
Bosko Danilovic <boskodanilovic@outlook.com>
2022-05-17 15:46:17 +0200

    TimeConversion

---
+++ TimeConversion/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace TimeConversion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            string result = Result.ConvertTimeToMilitary(s);
+            Console.WriteLine(result);
+        }
+    }
+
+    class Result 
+    {
+        public static string ConvertTimeToMilitary(string s)
+        {
+            bool succesfulConversion = DateTime.TryParse(s, out DateTime time);
+
+            if (succesfulConversion)
+            {
+                return time.ToString("HH:mm:ss"); // upperCase HH for military time
+            }
+
+            return "Bad input";
+        }
+    }
+}
