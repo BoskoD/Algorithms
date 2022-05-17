c78e033ea98073b376e840d0b7067ac6f26c7616
Bosko Danilovic <boskodanilovic@outlook.com>
2022-05-17 16:05:50 +0200

    CamelCase

---
+++ Strings/CamelCase/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace CamelCase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            int result = Result.CamelCase(s);
+
+            Console.WriteLine(result);
+        }
+    }
+
+    class Result
+    {
+        public static int CamelCase(string s)
+        {
+            var chars = s.ToCharArray();
+            int countOfWords = 0;
+
+            foreach (var item in chars)
+            {
+                if (char.IsUpper(item))
+                {
+                    countOfWords++;
+                }
+            }
+            countOfWords++;
+
+            return countOfWords;
+        }
+    }
+}
