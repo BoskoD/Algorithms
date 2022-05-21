d560c199fdacd82816de6a9fb4633e812d9957e9
Bosko Danilovic <boskodanilovic@outlook.com>
2022-05-21 18:31:57 +0200

    DivisibleSumPairs

---
+++ Implementation/DivisibleSumPairs/Program.cs
@@ -0,0 +1,38 @@
+﻿//https://www.hackerrank.com/challenges/divisible-sum-pairs/problem?isFullScreen=true&h_r=next-challenge&h_v=zen
+using System;
+
+namespace DivisibleSumPairs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] segment = new int[] { 1, 3, 2, 6, 1, 2 };
+            int k = 3;
+            int n = segment.Length;
+
+            int result = Result.DivisibleSumPairsCount(segment, k, n);
+            Console.WriteLine($"There are {result} pairs that can be divided by {k}");
+        }
+    }
+
+    internal class Result
+    {
+        internal static int DivisibleSumPairsCount(int[] segment, int k, int n)
+        {
+            int result = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = i + 1; j < n; j++)
+                {
+                    if ((segment[i] + segment[j]) % k == 0)
+                    {
+                        result++;
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
