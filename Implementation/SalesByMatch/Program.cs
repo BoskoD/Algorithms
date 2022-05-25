cc2b05869cc879acbdc3ef5d125bfd54b7d45afc
Bosko Danilovic <boskodanilovic@outlook.com>
2022-05-25 13:09:55 +0200

    SalesByMatch - Sock Merchant

---
+++ Implementation/SalesByMatch/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SalesByMatch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] socks = new int[] { 10, 20, 20, 10, 10, 30, 50, 10, 20 };
+            int size = socks.Length;
+            int pairs = Result.SockMerchant(size, socks);
+            Console.WriteLine(pairs);
+        }
+    }
+
+    internal class Result
+    {
+        internal static int SockMerchant(int size, int[] socks)
+        {
+            var colors = new HashSet<int>();
+            int pairs = 0;
+
+            for (int i = 0; i < size; i++)
+            {
+                if (!colors.Contains(socks[i]))
+                {
+                    colors.Add(socks[i]);
+                }
+                else 
+                {
+                    pairs++;
+                    colors.Remove(socks[i]);
+                }
+            }
+            return pairs;
+        }
+    }
+}
