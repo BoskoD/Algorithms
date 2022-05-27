a592d33e36ba956311036d38b6874e500762313c
Bosko Danilovic <boskodanilovic@outlook.com>
2022-05-27 14:22:16 +0200

    CountingValleys

---
+++ Implementation/CountingValleys/Program.cs
@@ -0,0 +1,42 @@
+﻿//https://www.hackerrank.com/challenges/counting-valleys/problem?isFullScreen=true
+using System;
+
+namespace CountingValleys
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int steps = Convert.ToInt32(Console.ReadLine());
+            string path = Console.ReadLine();
+            int valleysCount = Result.CountValleys(steps, path);
+            Console.WriteLine($"Count of valleys: {valleysCount}.");
+        }
+    }
+
+    internal class Result
+    {
+        internal static int CountValleys(int steps, string path)
+        {
+            int altitude = 0;
+            int valleys = 0;
+
+            for (int i = 0; i < steps; i++)
+            {
+                if (path[i] == 'U')
+                {
+                    if (altitude == -1)
+                    {
+                        valleys++;
+                    }
+                    altitude++;
+                }
+                if (path[i] == 'D')
+                {
+                    altitude--;
+                }
+            }
+            return valleys;
+        }
+    }
+}
