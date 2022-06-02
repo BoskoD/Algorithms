dce5ee6ed03aada55a9579e42dd9daa42ef21094
Bosko Danilovic <boskodanilovic@outlook.com>
2022-06-02 13:48:49 +0200

    PickingNumbers

---
+++ Implementation/PickingNumbers/Program.cs
@@ -0,0 +1,47 @@
+﻿//https://www.hackerrank.com/challenges/picking-numbers/problem?isFullScreen=true
+using System;
+using System.Collections.Generic;
+
+namespace PickingNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>{ 1, 1, 2, 2, 4, 4, 5, 5, 5 };
+            int longestSubArray = Result.PickingNumbers(list);
+            Console.WriteLine($"The maximum length subarray has {longestSubArray} elements.");
+        }
+    }
+
+    internal class Result
+    {
+        internal static int PickingNumbers(List<int> list)
+        {
+            int[] array = list.ToArray();
+            int max = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                int temp = 0;
+                Array.Sort(array);
+                for (int j = i; j < array.Length; j++)
+                {
+                    if (Math.Abs(array[i] - array[j]) <= 1)
+                    {
+                        temp++;
+                    }
+                    else 
+                    {
+                        break;
+                    }
+                }
+                if (temp > max) 
+                {
+                    max = temp;
+                }
+            }
+            return max;
+        }
+    }
+}
