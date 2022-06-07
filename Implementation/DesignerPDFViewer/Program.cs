ebc1ab243c68239f46980ef39f04f6adbf81f974
Bosko Danilovic <boskodanilovic@outlook.com>
2022-06-07 19:06:42 +0200

    DesignerPDFViewer

---
+++ Implementation/DesignerPDFViewer/Program.cs
@@ -0,0 +1,44 @@
+﻿//https://www.hackerrank.com/challenges/designer-pdf-viewer/problem?isFullScreen=true
+using System;
+using System.Collections.Generic;
+
+namespace DesignerPDFViewer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] alphabetHeight = new int[] { 1, 3, 1, 3, 1, 4, 1, 3, 2, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 7 };
+            string word = "zaba";
+
+            int hightlightedWord = Result.DesignerPDFView(alphabetHeight, word);
+            Console.WriteLine(hightlightedWord);
+        }
+    }
+
+    internal class Result
+    {
+        internal static int DesignerPDFView(int[] inputHeights, string word)
+        {
+            var alphabet = 'a';
+            var alphabetDictionary = new Dictionary<char, int>();
+
+            foreach (var item in inputHeights)
+            {
+                alphabetDictionary.Add(alphabet++, item);
+            }
+
+            var maxAlphabetHeight = 0;
+
+            foreach (var character in word)
+            {
+                if (alphabetDictionary[character] > maxAlphabetHeight)
+                {
+                    maxAlphabetHeight = alphabetDictionary[character];
+                }
+            }
+            var areaOfSelectedText = maxAlphabetHeight * word.Length;
+            return areaOfSelectedText;
+        }
+    }
+}
