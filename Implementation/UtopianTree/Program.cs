554d5992e9568f8406daf16e1850cfc8f2818ecc
Bosko Danilovic <boskodanilovic@outlook.com>
2022-06-08 17:41:47 +0200

    UtopianTree

---
+++ Implementation/UtopianTree/Program.cs
@@ -0,0 +1,43 @@
+﻿//https://www.hackerrank.com/challenges/utopian-tree/problem?isFullScreen=true
+using System;
+
+namespace UtopianTree
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = Convert.ToInt32(Console.ReadLine().Trim());
+
+            for (int iteration = 0; iteration < t; iteration++)
+            {
+                int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+                int result = Result.UtopianTree(n);
+
+                Console.WriteLine(result);
+            }
+        }
+    }
+
+    internal class Result
+    {
+        internal static int UtopianTree(int n)
+        {
+            int height = 1;
+
+            for (int cycles = 1; cycles <= n; cycles++)
+            {
+                if (cycles % 2 == 1)
+                {
+                    height *= 2;
+                }
+                else
+                {
+                    height++;
+                }
+            }
+            return height;
+        }
+    }
+}
