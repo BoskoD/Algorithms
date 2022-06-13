5a08ec5aee62294ca13b15a7e76d9672d48e68be
Bosko Danilovic <boskodanilovic@outlook.com>
2022-06-13 17:03:35 +0200

    BestTimeToByAndSellStock-LeetCode

---
+++ Implementation/BestTimeToBuyAndSellStock/Program.cs
@@ -0,0 +1,37 @@
+﻿//https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+using System;
+
+namespace BestTimeToBuyAndSellStock
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] prices = new int[] { 7, 1, 5, 3, 6, 4 };
+            int maxProfitWeCanAchieve = Result.MaxProfit(prices);
+            Console.WriteLine(maxProfitWeCanAchieve);
+        }
+    }
+
+    internal class Result
+    {
+        internal static int MaxProfit(int[] prices)
+        {
+            int maxProfit = 0;
+            int minPrice = Int32.MaxValue;
+
+            for (int i = 0; i < prices.Length; i++)
+            {
+                if (minPrice > prices[i])
+                {
+                    minPrice = prices[i];
+                }
+                else if (prices[i] - minPrice > maxProfit)
+                {
+                    maxProfit = prices[i] - minPrice;
+                }
+            }
+            return maxProfit;
+        }
+    }
+}
