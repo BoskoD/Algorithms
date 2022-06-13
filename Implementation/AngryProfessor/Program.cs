b0be0267b9cc900d5fc7553d1d729becce0f6d48
Bosko Danilovic <boskodanilovic@outlook.com>
2022-06-13 16:17:40 +0200

    AngryProfessor

---
+++ Implementation/AngryProfessor/Program.cs
@@ -0,0 +1,32 @@
+﻿//https://www.hackerrank.com/challenges/angry-professor/problem?isFullScreen=true
+using System;
+using System.Collections.Generic;
+
+namespace AngryProfessor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> arrivalTimes = new List<int> { -1, -3, 4, 2 };
+            int thresholdForTheClassToBeHeld = 3;
+
+            string willTheClassBeCancelled = Result.AngryProf(thresholdForTheClassToBeHeld, arrivalTimes);
+            Console.WriteLine(willTheClassBeCancelled);
+        }
+    }
+
+    internal class Result
+    {
+        internal static string AngryProf(int k, List<int> arrivalTimes)
+        {
+            int studentsArrivedOnTime = 0;
+
+            foreach (int std in arrivalTimes)
+            {
+                if (std <= 0) studentsArrivedOnTime++;
+            }
+            return studentsArrivedOnTime >= k ? "NO" : "YES";
+        }
+    }
+}
