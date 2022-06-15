81c23800c54245a62a6806385ab966b6e036357b
Bosko Danilovic <boskodanilovic@outlook.com>
2022-06-15 18:06:05 +0200

    BigSorting

---
+++ Sorting/BigSorting/Program.cs
@@ -0,0 +1,23 @@
+﻿//https://www.hackerrank.com/challenges/big-sorting/problem?isFullScreen=true
+using System;
+
+namespace BigSorting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] unsorted = new string[] { "120", "1", "20", "40" };
+
+            Array.Sort(unsorted, (string a, string b) => 
+            {
+                if (a.Length == b.Length)
+                {
+                    return string.Compare(a, b, StringComparison.Ordinal);
+                }
+                return a.Length - b.Length;
+            });
+            Console.WriteLine(string.Join("\n", unsorted));
+        }
+    }
+}
