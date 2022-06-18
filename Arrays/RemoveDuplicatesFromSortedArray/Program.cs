54b0d12fc593db527e925ba0343c28b88e5eaccc
Bosko Danilovic <boskodanilovic@outlook.com>
2022-06-18 11:34:38 +0200

    RemoveDuplicatesFromSortedArray

---
+++ Arrays/RemoveDuplicatesFromSortedArray/Program.cs
@@ -0,0 +1,40 @@
+﻿//https://leetcode.com/explore/featured/card/top-interview-questions-easy/92/array/727/
+using System;
+
+namespace RemoveDuplicatesFromSortedArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[10] { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
+            int length = Result.RemoveDuplicates(numbers);
+            Console.WriteLine(length);
+        }
+    }
+
+    internal class Result
+    {
+        internal static int RemoveDuplicates(int[] numbers)
+        {
+            if (numbers.Length == 0) 
+            {
+                return 0;
+            }
+
+            int previous = numbers[0];
+            int currentIndex = 0;
+
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                if (previous != numbers[i]) 
+                {
+                    currentIndex++;
+                    numbers[currentIndex] = numbers[i];
+                    previous = numbers[i];
+                }
+            }
+            return currentIndex + 1;
+        }
+    }
+}
