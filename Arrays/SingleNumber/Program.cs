ea3e4a6dbc3c704e21a0135edd384861fbc27556
Bosko Danilovic <boskodanilovic@outlook.com>
2022-06-26 14:20:56 +0200

    SingleNumber

---
+++ Arrays/SingleNumber/Program.cs
@@ -0,0 +1,24 @@
+﻿//https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/549/
+using System;
+using System.Linq;
+
+namespace SingleNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 4, 1, 2, 1, 2 };
+            var result = Result.SingleNumber(nums);
+            Console.WriteLine(result);
+        }
+    }
+
+    internal class Result
+    {
+        internal static object SingleNumber(int[] nums)
+        {
+            return nums.Aggregate(0, (current, i) => current ^ i);
+        }
+    }
+}
