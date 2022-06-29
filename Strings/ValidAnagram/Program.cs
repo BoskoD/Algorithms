868bc70d0244350d64f416489ffe54b2be6cab73
Bosko Danilovic <boskodanilovic@outlook.com>
2022-06-29 15:57:22 +0200

    Valid Anagram

---
+++ Strings/ValidAnagram/Program.cs
@@ -0,0 +1,44 @@
+﻿//https://leetcode.com/explore/interview/card/top-interview-questions-easy/127/strings/882/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ValidAnagram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstWord = "anagram";
+            string secondWord = "nagaram";
+            bool result = Result.IsAnagram(firstWord, secondWord);
+            Console.WriteLine(result);
+        }
+    }
+
+    internal class Result
+    {
+        internal static bool IsAnagram(string firstWord, string secondWord)
+        {
+            if (firstWord.Length != secondWord.Length) return false;
+            var dict = new Dictionary<char, int>();
+            foreach (var item in firstWord)
+            {
+                dict[item] = dict.ContainsKey(item) ? (dict[item] + 1) : 1;
+            }
+            foreach (var item in secondWord)
+            {
+                if (dict.ContainsKey(item))
+                {
+                    dict[item] -= 1;
+                    if (dict[item] < 0) return false;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return dict.All(d => d.Value == 0);
+        }
+    }
+}
