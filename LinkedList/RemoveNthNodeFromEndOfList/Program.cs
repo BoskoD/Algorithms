bd9ca9b4a42668b1bc25b75e57a6842ebd66a594
Bosko Danilovic <boskodanilovic@outlook.com>
2022-07-04 15:04:01 +0200

    Remove Nth Node from the end of the Linked List

---
+++ LinkedList/RemoveNthNodeFromEndOfList/Program.cs
@@ -0,0 +1,105 @@
+﻿//https://leetcode.com/explore/interview/card/top-interview-questions-easy/93/linked-list/603/
+using System;
+
+namespace RemoveNthNodeFromEndOfList
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList list = new LinkedList();
+            list.AddAtHead(5);
+            list.AddAtHead(4);
+            list.AddAtHead(3);
+            list.AddAtHead(2);
+            list.AddAtHead(1);
+            
+            Console.WriteLine("Before removal");
+            list.Print();
+
+            Console.WriteLine("After removal");
+            var head = list.head;
+            list.RemoveNthFromEnd(head, 2);
+            list.Print();
+        }
+    }
+
+    public class ListNode
+    {
+        public int Data { get; set; }
+        public ListNode Next { get; set; }
+
+        public ListNode(int data)
+        {
+            Data = data;
+            Next = null;
+        }
+    }
+
+    public class LinkedList
+    {
+        public ListNode head;
+        public ListNode tail;
+        int size = 0;
+
+        public void Print()
+        {
+            ListNode current = head;
+
+            if (head == null)
+            {
+                Console.WriteLine("Linked List is empty");
+                return;
+            }
+
+            while (current != null)
+            {
+                Console.WriteLine(current.Data);
+                current = current.Next;
+            }
+        }
+
+        public void AddAtHead(int data)
+        {
+            ListNode node = new ListNode(data);
+            if (size == 0) head = tail = node;
+            else
+            {
+                node.Next = head;
+                head = node;
+            }
+            size++;
+        }
+
+        public int SearchValue(int value)
+        {
+            if (head == null) return -1;
+            ListNode current = head;
+            int index = 0;
+            while (current != null)
+            {
+                if (current.Data == value) return index;
+                index++;
+                current = current.Next;
+            }
+            return -1;
+        }
+
+        public ListNode RemoveNthFromEnd(ListNode head, int n)
+        {
+            var left = head;
+            var right = head;
+
+            while (right != null)
+            {
+                right = right.Next;
+                if (n-- < 0) left = left.Next;
+            }
+
+            if (n == 0) head = head.Next;
+            else left.Next = left.Next.Next;
+
+            return head;
+        }
+    }
+}
---
+++ Tests/AlgorithmUnitTests/RemoveNthNodeFromEndOfTheLinkedList.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RemoveNthNodeFromEndOfList;
+
+namespace AlgorithmUnitTests
+{
+    [TestClass]
+    public class RemoveNthNodeFromEndOfTheLinkedList
+    {
+        [TestMethod]
+        public void RemoveNthNode_Returns_Head_Data()
+        {
+            //Arrange
+            int expected = 1;
+
+            LinkedList list = new LinkedList();
+            list.AddAtHead(5);
+            list.AddAtHead(4);
+            list.AddAtHead(3);
+            list.AddAtHead(2);
+            list.AddAtHead(1);
+
+            var headNode = list.head;
+            var n = 2;
+
+            //Act
+            list.RemoveNthFromEnd(headNode, n);
+
+            //Assert
+            int actual = list.RemoveNthFromEnd(headNode, n).Data;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void RemoveNthNode_SearchForRemovedNode_Returns_Minus1()
+        {
+            //Arrange
+            int expected = -1;
+
+            LinkedList list = new LinkedList();
+            list.AddAtHead(5);
+            list.AddAtHead(4);
+            list.AddAtHead(3);
+            list.AddAtHead(2);
+            list.AddAtHead(1);
+
+            var headNode = list.head;
+            var n = 2;
+
+            //Act
+            list.RemoveNthFromEnd(headNode, n);
+
+            //Assert
+            int actual = list.SearchValue(4);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void RemoveNthNode_SearchForTailIndex_Returns_3()
+        {
+            //Arrange
+            int expected = 3;
+
+            LinkedList list = new LinkedList();
+            list.AddAtHead(5);
+            list.AddAtHead(4);
+            list.AddAtHead(3);
+            list.AddAtHead(2);
+            list.AddAtHead(1);
+
+            var headNode = list.head;
+            var n = 2;
+
+            //Act
+            list.RemoveNthFromEnd(headNode, n);
+
+            //Assert
+            int actual = list.SearchValue(5);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
