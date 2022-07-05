ee49bd01a71aabcbd3252b510ceee254a0f1aa40
Bosko Danilovic <boskodanilovic@outlook.com>
2022-07-05 21:13:54 +0200

    Implement Str - Find a needle in a haystack

---
+++ Strings/ImplementStrStr/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ImplementStrStr
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string haystack = "hello";
+            string needle = "ll";
+            var indexOfNeedle = Result.StrStr(haystack, needle);
+            Console.WriteLine($"Needle found at index {indexOfNeedle}");
+        }
+    }
+
+    public static class Result
+    {
+        public static int StrStr(string haystack, string needle)
+        {
+            if (haystack.Contains(needle))
+            {
+                return haystack.IndexOf(needle);
+            }
+            return -1;
+        }
+    }
+}
---
+++ Tests/AlgorithmUnitTests/ImplementStr.cs
@@ -0,0 +1,41 @@
+﻿//https://leetcode.com/explore/interview/card/top-interview-questions-easy/127/strings/885/
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlgorithmUnitTests
+{
+    [TestClass]
+    public class ImplementStr
+    {
+        [TestMethod]
+        public void FindNeedleInAHaystack_Returns_CorrectIndex()
+        {
+            //Arrange
+            int expected = 2;
+            string haystack = "hello";
+            string needle = "ll";
+
+            //Act
+            ImplementStrStr.Result.StrStr(haystack, needle);
+
+            //Assert
+            var actual = ImplementStrStr.Result.StrStr(haystack, needle);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void FindNeedleInAHaystack_NeedleNotInHaystack_Returns_Minus1()
+        {
+            //Arrange
+            int expected = -1;
+            string haystack = "howdy";
+            string needle = "ll";
+
+            //Act
+            ImplementStrStr.Result.StrStr(haystack, needle);
+
+            //Assert
+            var actual = ImplementStrStr.Result.StrStr(haystack, needle);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
