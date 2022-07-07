04f94bcbf1f72c38ad936fe4d5471b74492df10a
Bosko Danilovic <boskodanilovic@outlook.com>
2022-07-07 14:49:55 +0200

    Intersection of Two Arrays

---
+++ Arrays/IntersectionOfTwoArrays/Program.cs
@@ -0,0 +1,34 @@
+﻿//https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/674/
+using System;
+using System.Linq;
+
+namespace IntersectionOfTwoArrays
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr1 = new int[] { 1, 2, 2, 1 };
+            int[] arr2 = new int[] { 2, 2, 3 };
+            var result = Result.FindIntersection(arr1, arr2);
+            Result.Print(result);
+        }
+    }
+
+    public class Result
+    {
+        public static int[] FindIntersection(int[] arr1, int[] arr2)
+        {
+            var map1 = arr1.GroupBy(n => n).ToDictionary(g => g.Key, g => g.Count());
+            return arr2.Where(n => map1.ContainsKey(n) && map1[n]-- > 0).ToArray();
+        }
+
+        public static void Print(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i]);
+            }
+        }
+    }
+}
---
+++ Tests/AlgorithmUnitTests/FindIntersectionOfTwoArrays.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlgorithmUnitTests
+{
+    [TestClass]
+    public class FindIntersectionOfTwoArrays
+    {
+        [TestMethod]
+        public void IntersectionOfTwoArrays_Returns_Correct()
+        {
+            //Arrange
+            int[] arr1 = new int[] { 1, 2, 3, 7, 8 };
+            int[] arr2 = new int[] { 2, 7, 9, 9, 0 };
+            int[] expected = new int[] { 2, 7 };
+
+            //Act
+            IntersectionOfTwoArrays.Result.FindIntersection(arr1, arr2);
+
+            //Assert
+            var actual = IntersectionOfTwoArrays.Result.FindIntersection(arr1, arr2);
+            Assert.AreEqual(expected.Length, actual.Length);
+            Assert.IsTrue((expected[0] == actual[0]) && (expected[^1] == actual[^1]));
+        }
+    }
+}
