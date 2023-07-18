9e307b35fa0c555f06267144d1abf7ff8a26c6f7
Bosko Danilovic <v-bodani@microsoft.com>
2023-07-18 19:09:07 +0200

    Add project files.

---
+++ TwoSum/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace TwoSum
+{
+    internal class Program // Array, HashTable
+    {
+        static void Main(string[] args)
+        {
+            var nums = new int[] { 2, 3, 4, 7, 10 };
+            var target = 11;
+            var result = CalculateTwoSum(nums, target);
+            Console.WriteLine($"Index 0: {result[0]}");
+            Console.WriteLine($"Index 1: {result[1]}");
+        }
+
+        public static int[] CalculateTwoSum(int[] nums, int target)
+        {
+            if (nums == null || nums.Length < 2)
+            {
+                return new int[2];
+            }
+
+            Dictionary<int, int> previousMap = new();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (previousMap.ContainsKey(target - nums[i]))
+                {
+                    return new int[] { previousMap[target - nums[i]], i };
+                }
+                else if (!previousMap.ContainsKey(target - nums[i]))
+                {
+                    previousMap.Add(nums[i], i);
+                }
+            }
+            return new int[2];
+        }
+    }
+}
---
+++ ValidParentheses/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace ValidParentheses
+{
+    internal class Program // String, Stack
+    {
+        static void Main(string[] args)
+        {
+            var input = "{[()]}";
+            var isValid = IsValidParentheses(input);
+            Console.WriteLine($"Order of parentheses is valid: {isValid}");
+        }
+
+        public static bool IsValidParentheses(string input)
+        {
+            var mapping = new Dictionary<char, char>
+            {
+                { '(', ')'},
+                { '[', ']'},
+                { '{', '}'}
+            };
+
+            Stack<char> openBrackets = new();
+
+            foreach (var c in input)
+            {
+                if (mapping.ContainsKey(c))
+                {
+                    openBrackets.Push(c);
+                }
+                else
+                {
+                    if (openBrackets.Count == 0) { return false; }
+                    if (mapping[openBrackets.Pop()] == c) { continue; }
+                    return false;
+                }
+            }
+            return openBrackets.Count == 0;
+        }
+    }
+}
