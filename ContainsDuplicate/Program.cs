aea54fd0a147c8d49e3677716c48a4c68aa5d286
Bosko Danilovic <v-bodani@microsoft.com>
2023-07-20 14:36:08 +0200

    Contains Duplicate

---
+++ ContainsDuplicate/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ContainsDuplicate
+{
+    internal class Program // Array, HashTable, Sorting
+    {
+        static void Main(string[] args)
+        {
+            var nums = new int[] { 2, 43, 45, 6 };
+            var containsDuplicate = DetectectDuplicate(nums);
+            Console.WriteLine($"Provided array contains duplicate? {containsDuplicate}");
+        }
+
+        public static bool DetectectDuplicate(int[] nums)
+        {
+            if (nums is not null && nums.Length < 2) { return false; }
+
+            HashSet<int> set = new();
+            foreach (var num in nums)
+            {
+                if (set.Contains(num)) { return true; }
+                if (!set.Contains(num)) { set.Add(num); }
+            }
+            return false;
+        }
+    }
+}
