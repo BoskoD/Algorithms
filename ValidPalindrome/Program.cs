38df0acc9a7936865e455b4962dff26ad2b46417
Bosko Danilovic <v-bodani@microsoft.com>
2023-07-21 23:52:53 +0200

    ValidPalindrome

---
+++ ValidPalindrome/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace ValidPalindrome
+{
+    internal class Program // Two Pointers, String
+    {
+        static void Main(string[] args)
+        {
+            string s = "A man, a plan, a canal: Panama";
+            bool isValidPalindrome = DetectValidPalindrome(s);
+            Console.WriteLine($"{s} is a valid palindrome? {isValidPalindrome}");
+        }
+
+        public static bool DetectValidPalindrome(string s)
+        { 
+            int left = 0, right = s.Length - 1;
+            while (left < right)
+            {
+                if (!char.IsLetterOrDigit(s[left])) { left++; }
+                else if (!char.IsLetterOrDigit(s[right])) { right--; }
+                else
+                {
+                    if (char.ToLower(s[left]) != char.ToLower(s[right])) 
+                    { 
+                        return false; 
+                    }
+                    left++;
+                    right--;
+                }
+            }
+            return true;
+        }
+    }
+}
