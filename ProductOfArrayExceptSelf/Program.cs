68035b4bccd0c24a5f181ca1bd2c71173d1fdc34
Bosko Danilovic <v-bodani@microsoft.com>
2023-07-24 22:49:26 +0200

    ProductOfArrayExceptSelf

---
+++ ProductOfArrayExceptSelf/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace ProductOfArrayExceptSelf
+{
+    internal class Program // Array, Prefix Sum
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 1, 2, 3, 4 };
+            int[] result = CalculateProductOfArraysExceptSelf(nums);
+            for (int i = 0; i < result.Length; i++)
+            {
+                Console.Write($"{result[i]} ");
+            }
+        }
+
+        public static int[] CalculateProductOfArraysExceptSelf(int[] nums)
+        {
+            int prefix = 1;
+            int postfix = 1;
+            int[] result = new int[nums.Length];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                result[i] = prefix;
+                prefix *= nums[i];
+            }
+
+            for (int i = nums.Length - 1; i > 0; i--) 
+            {
+                result[i] *= postfix;
+                postfix *= nums[i];
+            }
+            return result; 
+        }
+    }
+}
