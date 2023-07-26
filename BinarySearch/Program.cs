f936770458bc64e8dad9ce01f123d070e1788d3f
Bosko Danilovic <v-bodani@microsoft.com>
2023-07-26 17:19:57 +0200

    Binary Search

---
+++ BinarySearch/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace BinarySearch
+{
+    internal class Program // Array, Binary Search
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { -1, 0, 3, 5, 9, 12 };
+            int target = 9;
+            int result = BinarySearchTarget(nums, target);
+            Console.WriteLine($"{target} exists in nums and its index is {result}.");
+        }
+
+        public static int BinarySearchTarget(int[] nums, int target)
+        {
+            int left = 0;
+            int right = nums.Length - 1;
+
+            while (left <= right)
+            {
+                int mid = left + ((right - left) / 2);
+                if (nums[mid] > target) right = mid - 1;
+                else if (nums[mid] < target) left = mid + 1;
+                else
+                {
+                    return mid;
+                }
+            }
+            return -1;
+        }
+    }
+}
