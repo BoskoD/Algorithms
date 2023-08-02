df756cdef7c831ac7d742b8711905d4d699227ac
Bosko Danilovic <v-bodani@microsoft.com>
2023-08-02 19:58:31 +0200

    Trapping Rain Water

---
+++ TrappingRainWater/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace TrappingRainWater
+{
+    internal class Program // Array, Two Pinters, Dynamic Programming
+    {
+        static void Main(string[] args)
+        {
+            int[] height = { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 };
+            int trappedWawterUnits = TrapWater(height);
+            Console.WriteLine($"We can trap {trappedWawterUnits} units of water.");
+        }
+
+        public static int TrapWater(int[] height)
+        {
+            if (height == null || height.Length == 0) return 0;
+
+            int left = 0;
+            int right = height.Length - 1;
+
+            int maxLeft = height[left];
+            int maxRight = height[right];
+
+            int result = 0;
+
+            while(left < right)
+            {
+                if (height[left] < height[right])
+                {
+                    left++;
+                    maxLeft = Math.Max(maxLeft, height[left]);
+                    result += maxLeft - height[left];
+                }
+                else
+                {
+                    maxRight = Math.Max(maxRight, height[right]);
+                    result += maxRight - height[right];
+                    right--;
+                }
+            }
+            return result;
+        }
+    }
+}
