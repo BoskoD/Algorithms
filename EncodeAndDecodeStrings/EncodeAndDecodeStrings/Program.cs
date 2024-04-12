01d83a1e61e6f27461dab1a2e4b063eb5817397c
Bosko Danilovic <v-bodani@microsoft.com>
2024-04-12 18:21:44 +0200

    EncodeAndDecodeStrings

---
+++ EncodeAndDecodeStrings/EncodeAndDecodeStrings/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace EncodeAndDecodeStrings
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = ["lint", "code", "love", "you"];
+
+            string encoded = Encode(input);
+            Console.WriteLine($"\nEncoded: {encoded}\n");
+            
+            Console.WriteLine("\nDecoded:\n");
+            IList<string> decoded = Decode(encoded);
+            foreach (string item in decoded)
+            {
+                Console.Write(item);
+            }
+        }
+
+        public static string Encode(IList<string> str)
+        {
+            return string.Concat(str.SelectMany(s => $"{s.Length}#{s}"));
+        }
+
+        public static IList<string> Decode(string input) // 4#lint4#code4#love3#you
+        {
+            IList<string> result = [];
+
+            int i = 0;
+            while (i < input.Length)
+            {
+                int j = i;
+                while (input[j] != '#')
+                {
+                    ++j;
+                }
+
+                _ = int.TryParse(input.AsSpan(i, j - i), out int len);
+                j++;
+                result.Add(input.Substring(j, len));
+                i = j + len;
+            }
+            
+            return result;
+        }
+    }
+}
