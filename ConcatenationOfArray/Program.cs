c6a1f9ecded304565ffe6f7559593a010d5fe7b0
Bosko Danilovic <v-bodani@microsoft.com>
2024-04-15 18:51:26 +0200

    Array

---
+++ ConcatenationOfArray/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ConcatenationOfArray
+{
+    internal class Program // Array
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = [1, 2, 1];
+            int[] result = GetConcatenation(nums);
+            for (int i = 0; i < result.Length; i++)
+            {
+                Console.WriteLine(result[i]);
+            }
+        }
+
+        public static int[] GetConcatenation(int[]nums)
+        {
+            int[] result = new int[nums.Length * 2];
+
+            for(int i = 0; i < nums.Length; i++)
+            {
+                result[i] = nums[i];
+                result[nums.Length + i] = nums[i];
+            }
+            return result;
+        }
+    }
+}
