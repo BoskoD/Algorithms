e5999d39b3a53a8869a692f391e9912f185354df
Bosko Danilovic <v-bodani@microsoft.com>
2024-04-16 20:58:18 +0200

    Array, Matrix

---
+++ RichestCustomerWealth/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace RichestCustomerWealth
+{
+    internal class Program // Array, Matrix
+    {
+        static void Main(string[] args)
+        {
+            int[][] accounts = [[1, 5], [8, 3], [3, 5]];
+            int richestAccount = RichestCustomerWealthLINQ(accounts);
+            Console.WriteLine(richestAccount);
+        }
+
+        public static int RichestCustomerWealth(int[][] accounts)
+        {
+            int richestAccount = 0;
+            for (int i = 0; i < accounts.Length; i++)
+            {
+                int maxValue = 0;
+                foreach (int val in accounts[i])
+                {
+                    maxValue += val;
+                }
+                richestAccount = Math.Max(richestAccount, maxValue);
+            }
+            return richestAccount;
+        }
+
+        public static int RichestCustomerWealthLINQ(int[][] accounts)
+        {
+            return accounts.Max(account => account.Sum());
+        }
+    }
+}
