6a14834af752283029eab1308d44d9de3140cc05
Bosko Danilovic <v-bodani@microsoft.com>
2024-04-18 16:46:06 +0200

    Arrays, Dynamic Programming

---
+++ CoinChange2/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace CoinChange2
+{
+    internal class Program // Arrays, Dynamic Programming
+    {
+        static void Main(string[] args)
+        {
+            int amount = 5;
+            int[] coins = [1, 2, 5];
+            int result = CoinChangeCombinations(coins, amount);
+            Console.WriteLine(result);
+        }
+
+        public static int CoinChangeCombinations(int[] coins, int amount)
+        {
+            int[] combinations = new int[amount + 1];
+            combinations[0] = 1; // base case
+
+            foreach (int coin in coins)
+            {
+                for (int i = coin; i <= amount; i++)
+                {
+                    combinations[i] += combinations[i - coin];
+                }
+            }
+            return combinations[amount];
+        }
+    }
+}
